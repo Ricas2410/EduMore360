5f67cbff48b8fcdcdc6cdd0128ddc52e08c6306c
edumore <edumore360.edumore@colaname.com>
2025-05-17 22:20:29 +0000

    new changes

--- edumore360/settings.py
+++ edumore360/settings.py
@@ -12,9 +12,6 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
